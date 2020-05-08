37b740c2e95320c45e1e5a3071679ded0052f967
Ayaka Hara <ayhara@microsoft.com>
2020-05-08 21:50:29 +0900

    build a music genre classification model with Keras

---
+++ music-genre-classification-keras.py
@@ -0,0 +1,179 @@
+import numpy as np
+import os
+from os.path import isfile
+
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Input, Dense, TimeDistributed, LSTM, Dropout, Activation
+from keras.layers import Conv1D, MaxPooling1D, Flatten, Conv2D, BatchNormalization, Lambda
+from keras.layers.advanced_activations import ELU
+from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
+from keras import backend
+from keras.utils import np_utils
+from keras.optimizers import Adam, RMSprop
+from keras import regularizers
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+import seaborn as sns
+
+import librosa
+import librosa.display
+
+import matplotlib.pyplot as plt
+
+
+# Load training arrays
+dict_genres = {'Electronic': 0, 'Experimental': 1, 'Folk': 2, 'Hip-Hop': 3,
+               'Instrumental': 4, 'International': 5, 'Pop': 6, 'Rock': 7}
+
+reverse_map = {v: k for k, v in dict_genres.items()}
+
+npzfile = np.load('shuffled_train.npz')
+X_train = npzfile['arr_0']
+y_train = npzfile['arr_1']
+
+# Load validation arrays
+npzfile = np.load('shuffled_valid.npz')
+X_valid = npzfile['arr_0']
+y_valid = npzfile['arr_1']
+
+
+# Train model
+def conv_recurrent_model_build(model_input):
+    print('Building model...')
+    layer = model_input
+
+    ### 3 1D Convolution Layers
+    for i in range(N_LAYERS):
+        # give name to the layers
+        layer = Conv1D(
+            filters=CONV_FILTER_COUNT,
+            kernel_size=FILTER_LENGTH,
+            kernel_regularizer=regularizers.l2(
+                L2_regularization),  # Tried 0.001
+            name='convolution_' + str(i + 1)
+        )(layer)
+        layer = BatchNormalization(momentum=0.9)(layer)
+        layer = Activation('relu')(layer)
+        layer = MaxPooling1D(2)(layer)
+        layer = Dropout(0.4)(layer)
+
+    ## LSTM Layer
+    layer = LSTM(LSTM_COUNT, return_sequences=False)(layer)
+    layer = Dropout(0.4)(layer)
+
+    ## Dense Layer
+    layer = Dense(NUM_HIDDEN, kernel_regularizer=regularizers.l2(
+        L2_regularization), name='dense1')(layer)
+    layer = Dropout(0.4)(layer)
+
+    ## Softmax Output
+    layer = Dense(num_classes)(layer)
+    layer = Activation('softmax', name='output_realtime')(layer)
+    model_output = layer
+    model = Model(model_input, model_output)
+
+    opt = Adam(lr=0.001)
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=opt,
+        metrics=['accuracy']
+    )
+
+    print(model.summary())
+    return model
+
+
+def train_model(x_train, y_train, x_val, y_val):
+
+    n_features = x_train.shape[2]
+    input_shape = (None, n_features)
+    model_input = Input(input_shape, name='input')
+
+    model = conv_recurrent_model_build(model_input)
+
+    checkpoint_callback = ModelCheckpoint('./models/crnn/weights.best.h5', monitor='val_accuracy', verbose=1,
+                                          save_best_only=True, mode='max')
+
+    reducelr_callback = ReduceLROnPlateau(
+                monitor='val_accuracy', factor=0.5, patience=10, min_delta=0.01,
+                verbose=1
+            )
+    callbacks_list = [checkpoint_callback, reducelr_callback]
+
+    # Fit the model and get training history.
+    print('Training...')
+    history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCH_COUNT,
+                        validation_data=(x_val, y_val), verbose=1, callbacks=callbacks_list)
+
+    return model, history
+
+
+def show_summary_stats(history):
+    # List all data in history
+    print(history.history.keys())
+
+    # Summarize history for accuracy
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    # Summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+batch_size = 32
+num_classes = 8
+n_features = X_train.shape[2]
+n_time = X_train.shape[1]
+
+N_LAYERS = 3
+FILTER_LENGTH = 5
+CONV_FILTER_COUNT = 56
+BATCH_SIZE = 32
+LSTM_COUNT = 96
+EPOCH_COUNT = 70
+NUM_HIDDEN = 64
+L2_regularization = 0.001
+
+model, history = train_model(X_train, y_train, X_valid, y_valid)
+
+# Show model accuracy and loss
+show_summary_stats(history)
+
+
+# Show classification report
+y_true = np.argmax(y_valid, axis = 1)
+y_pred = model.predict(X_valid)
+y_pred = np.argmax(y_pred, axis=1)
+labels = [0,1,2,3,4,5,6,7]
+target_names = dict_genres.keys()
+
+print(y_true.shape, y_pred.shape)
+print(classification_report(y_true, y_pred, target_names=target_names))
+
+
+# Show accuracy score
+print(accuracy_score(y_true, y_pred))
+
+
+# Show confusion matrix
+mat = confusion_matrix(y_true, y_pred)
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
+            xticklabels=dict_genres.keys(),
+            yticklabels=dict_genres.keys())
+plt.xlabel('Actual Label')
+plt.ylabel('Predicted Label')
